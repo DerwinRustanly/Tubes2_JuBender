74f09d2149e3306d5d6b24dd32134fb202d8c196
DerwinRustanly <117006907+DerwinRustanly@users.noreply.github.com>
2024-04-17 22:57:27 +0700

    feat: add finder page design

--- frontend/src/pages/finder.js
+++ frontend/src/pages/finder.js
@@ -1,4 +1,4 @@
-import swtch from "../assets/switch.svg"
+import swtch from "../assets/switch.svg";
 
 export default function Finder() {
   return (
@@ -15,28 +15,34 @@ export default function Finder() {
             </button>
           </div>
         </div>
-        <div className="flex flex-col justify-center text-3 mt-4 mb-2 py-4 px-8 bg-2 rounded-xl">
-          <span>From</span>
-          <form action="">
-            <input
-              type="text"
-              className="text-2xl font-bold border-none outline-none bg-transparent"
-              defaultValue={"Source"}
-            />
-          </form>
-          <span className="text-sm">en.wikipedia.org</span>
+        <div>
+          <img alt="" src={swtch} className="border-1 border-[6px] rounded-xl bg-2 absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></img>
+          <div className="relative flex flex-col justify-center text-3 mt-4 mb-2 py-4 px-8 bg-2 rounded-xl">
+            <span>From</span>
+            <form action="">
+              <input
+                type="text"
+                className="text-2xl font-bold border-none outline-none bg-transparent"
+                defaultValue={"Source"}
+              />
+            </form>
+            <span className="text-sm">en.wikipedia.org</span>
+          </div>
+          <div className="flex flex-col justify-center text-3 mb-2 py-4 px-8 bg-2 rounded-xl">
+            <span>To</span>
+            <form action="">
+              <input
+                type="text"
+                className="text-2xl font-bold border-none outline-none bg-transparent"
+                defaultValue={"Destination"}
+              />
+            </form>
+            <span className="text-sm">en.wikipedia.org</span>
+          </div>
         </div>
-        <div className="flex flex-col justify-center text-3 mb-4 py-4 px-8 bg-2 rounded-xl">
-          <span>To</span>
-          <form action="">
-            <input
-              type="text"
-              className="text-2xl font-bold border-none outline-none bg-transparent"
-              defaultValue={"Destination"}
-            />
-          </form>
-          <span className="text-sm">en.wikipedia.org</span>
-        </div>
+        <button className="bg-4 w-full text-5 flex justify-center px-4 py-2 rounded-xl font-bold">
+          Search Path
+        </button>
       </div>
     </div>
   );
