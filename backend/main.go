1ea1acd7be7ff57c025388f8d6c5a0f23f5b544e
Juan A <140235896+juanaw6@users.noreply.github.com>
2024-04-22 11:50:54 +0700

    fix: fix CORSmiddleware

--- backend/main.go
+++ backend/main.go
@@ -10,16 +10,12 @@ import (
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if c.Request.Method == "OPTIONS" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
-			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-			c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
-			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-			c.AbortWithStatus(http.StatusOK)
-		} else {
-			c.Next()
-		}
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.AbortWithStatus(http.StatusOK)
 	}
 }
 
